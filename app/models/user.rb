5fca5f7ff834aba2ed9bff402f448bf46fef3985
Hyun Kim <hkim@live.com.au>
2017-04-24 00:33:24 +1000

    lol

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,7 @@ class User < ApplicationRecord
   has_one :profile
   has_many :solutions, dependent: :destroy
 
+  lol
+
   has_many :challenges_with_solutions, :through => :solutions, :source => :challenge
 end
